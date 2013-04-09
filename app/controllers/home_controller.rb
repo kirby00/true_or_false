7b7b72fa13627e93794d830981fc3f95d18cbdc3
kirby00 <mbkirby@gmail.com>
2013-04-09 17:59:06 +0100

    user answer display on question list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
     @statements = Statement.all
     @statement = Statement.new
+    if current_user
+      @answers = Answer.find_all_by_user_id(session[:user_id])
+    end
   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,8 @@ class Answer < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :question_id, :presence => true
   validates :user_answer, :presence => true
+
+  def matches_current_user?(statement)
+    question_id == statement.id
+  end
 end
