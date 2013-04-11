90007b80163950a52807640fef1e4c9eff7e0ee2
kirby00 <mbkirby@gmail.com>
2013-04-11 18:01:35 +0100

    remove faked session logged in for localhost

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    session[:user_id] = 1
     @statements = Statement.latest
     @answers = Answer.user_answers(session[:user_id]) if current_user
   end
